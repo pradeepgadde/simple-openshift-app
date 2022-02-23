854c5cbc2f928e4d33c4a4663bac21e2138cab8a
Pradeep Gadde <63095349+pradeepgadde@users.noreply.github.com>
2022-02-23 11:55:47 +0530

    To test manual trigger of  OpenShift Build

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>GOEDEMIDDAG GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
+	
+        <h2>Greetings from Pradeep Gadde!</h2>
 	</body> 
 </html>
